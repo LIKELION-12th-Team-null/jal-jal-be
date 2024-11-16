25059a4b8c47bec0fa93de42fe6cdb6221d4a233
Keon <lune0410k@naver.com>
2024-11-16 14:49:30 +0900

    feat: add adjective and noun for nickname generator

--- src/main/java/com/example/demo/member/domain/NicknameGenerator.java
+++ src/main/java/com/example/demo/member/domain/NicknameGenerator.java
@@ -5,8 +5,10 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class NicknameGenerator {
 
-	private static final List<String> adjective = List.of("무거운", "귀여운", "졸린", "반가운", "행복한", "압도적인", "슬픈", "감정적인");
-	private static final List<String> noun = List.of("카피바라", "돌고래", "여우", "고양이", "강아지", "새우", "바퀴벌레", "코끼리");
+	private static final List<String> adjective = List.of("신비로운", "재빠른", "활기찬", "고요한", "귀여운",
+		"호기로운", "용감한", "기발한", "자유로운", "낯선", "조용한", "유쾌한", "차분한", "멋진", "슬기로운", "비범한", "강인한");
+	private static final List<String> noun = List.of("벨루가", "범고래", "흰동가리", "혹등고래", "고래상어", "망치상어", "청새치",
+		"돌고래", "듀공", "바다코끼리", "바다표범", "해마", "가오리", "물개", "뱀장어", "넙치");
 
 	public static String generateNickname() {
 		int adjectiveIndex = ThreadLocalRandom.current().nextInt(8);
